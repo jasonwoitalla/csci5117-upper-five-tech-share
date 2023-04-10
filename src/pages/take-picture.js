7df0b273a6a8f675fc912b63a89e6fa8a0aba0e7
Anwaar Hadi <anwaar.hadi@gmail.com>
2023-04-09 22:14:33 -0500

    react webcam in take-picture.js

--- src/pages/take-picture.js
+++ src/pages/take-picture.js
@@ -1,5 +1,25 @@
+import { useCloudUpload } from "@/hooks/useCloudStorage";
+import { useState, useEffect, useRef, useCallback } from "react";
+import Webcam from "react-webcam";
+
 function takePicture() {
-    return <></>;
+    const webcamRef = useRef(null)
+    const [image, setImage] = useState(null)
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot()
+        setImage(imageSrc)
+    }, [webcamRef, setImage])
+    async function uploadScreenshot() { await useCloudUpload(image) }
+
+    return 
+    ( 
+        <>
+            <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+            <button onClick={capture}>Get Screenshot</button>
+            {image && ( <img src={image} /> )}
+            <button onClick={uploadScreenshot}>Upload Screenshot to Gallery</button>
+        </>
+    )
 }
 
 export default takePicture;
